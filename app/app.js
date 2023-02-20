20974fbeac9b1ba050b314de2e72e4a285a0a5a6
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-20 16:38:14 +0000

    Creating error handling middlewares

--- app/app.js
+++ app/app.js
@@ -1,5 +1,17 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const {errorhandler, notFound} = require("../middleware/globalErrorhandler");
 const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req,res)=>{
+    res.send("Hello World");
+})
+
+//error handler middleware
+app.use(errorhandler);
+//not find Middleware
+app.use(notFound);
 
 module.exports = app;
---
+++ middleware/globalErrorhandler.js
@@ -0,0 +1,22 @@
+const errorhandler = (err, req, res, next) => {
+  //status
+  const status = err.status ? err.status : "Failed";
+  //massage
+  const message = err.message;
+  //stack
+  const stack = err.stack;
+  //status code
+  const statusCode = err.status ? err.statusCode : 500;
+  res.status(statusCode).json({
+    status,
+    message,
+    stack,
+  });
+};
+
+const notFound = (req,res,next) =>{
+    const err = new Error(`Cant't find ${req.OriginalUrl} on the server`);
+    next(err)
+}
+
+module.exports = { errorhandler , notFound};
