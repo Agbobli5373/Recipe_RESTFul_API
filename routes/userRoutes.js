61b00436cabef286728e8424bb20cf14f92199d0
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-21 12:50:49 +0000

    create function for jwt generating

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,51 +1,45 @@
 const express = require("express");
+const { registerCtrl } = require("../controller/userCtrl");
 
 const userRouter = express.Router();
 
-
 //Registration of user
-userRouter.post("/signup", async (req,res) => {
-    res.status(201).json({
-        status :"Success",
-        message : "User Sign up Successfull",
-        data : "User Details"
-    })
-})
+userRouter.post("/signup", registerCtrl);
 
-// User Login 
-userRouter.post("/login", async (req,res) => {
-    res.status(200).json({
-        status :"Success",
-        message : "User Login Successfull",
-        data : "User Details"
-    })
-})
+// User Login
+userRouter.post("/login", async (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "User Login Successfull",
+    data: "User Details",
+  });
+});
 
 //View user profile
-userRouter.get("/profile", async (req,res) => {
-    res.status(201).json({
-        status :"Success",
-        message : "Get User Profile Successfull",
-        data : "User Details"
-    })
-})
+userRouter.get("/profile", async (req, res) => {
+  res.status(201).json({
+    status: "Success",
+    message: "Get User Profile Successfull",
+    data: "User Details",
+  });
+});
 
 //update user profile
-userRouter.put("/:userID", async (req,res) => {
-    res.status(201).json({
-        status :"Success",
-        message : "Update User Successfull",
-        data : "User Details"
-    })
-})
+userRouter.put("/:userID", async (req, res) => {
+  res.status(201).json({
+    status: "Success",
+    message: "Update User Successfull",
+    data: "User Details",
+  });
+});
 
 //Delete user profile
-userRouter.delete("/:userID", async (req,res) => {
-    res.status(201).json({
-        status :"Success",
-        message : "User deleted Successfull",
-        data : "User Details"
-    })
-})
+userRouter.delete("/:userID", async (req, res) => {
+  res.status(201).json({
+    status: "Success",
+    message: "User deleted Successfull",
+    data: "User Details",
+  });
+});
 
-module.exports = userRouter ;
+module.exports = userRouter;
--- utils/helper.js
+++ utils/helper.js
@@ -1,14 +1,20 @@
+const AsyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 //Function for password hashing
-exports.hashPassword = async (password) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-  } catch (err) {
-    console.log(err);
-  }
-};
+exports.hashPassword = AsyncHandler(async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+});
+
 // Function for comparing password with hash password
-exports.verifyPassword = async (password, hashPasswod) => {
+exports.verifyPassword = AsyncHandler(async (password, hashPasswod) => {
   return await bcrypt.compare(password, hashPasswod);
-};
+});
+
+// Function for generating json web token
+exports.generateToken = AsyncHandler(async (id) => {
+  return await jwt.sign({ id }, process.env.secretKey, { expiresIn: "48h" });
+});
